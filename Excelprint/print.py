375597fdc706d6a8c8d707210de51406c54c736b
jijiyam <malody17120@naver.com>
2023-08-02 09:36:50 +0900

    dhxktnwjd

--- Excelprint/print.py
+++ Excelprint/print.py
@@ -1,3 +1,11 @@
 import xlrd
 
-workbook = xlrd.open_workbook('Excel'
+workbook = xlrd.open_workbook('C:/Users/user/Desktop/source (2)/Excel/singer.xls')
+sheetCount = workbook.nsheets
+print('워크시트는 %d개 입니다'%(sheetCount))
+
+wsheetList = workbook.sheets()
+for worksheet in wsheetList() : 
+    print('** 워크시트의 이름 : %s' % (worksheet.name))
+    print(" 행 수는 %d, 열 개수는 %d입니다." %(worksheet.nrows, worksheet.ncols) )
+
