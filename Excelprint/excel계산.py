e24698d325a5764d79613e425bbd50f00bc3f5ce
jijiyam <malody17120@naver.com>
2023-08-02 10:14:05 +0900

    주석에 내가 이해 안되는 거 적어둠

--- Excelprint/excel계산.py
+++ Excelprint/excel계산.py
@@ -1,9 +1,9 @@
 import xlrd
 
-workbook = xlrd.open_workbook('singer.xls')
-sheetCount = workbook.nsheets
+workbook = xlrd.open_workbook('singer.xls') #xlrd = excel read, 엑셀 파일을 읽어서 데이터에 넣는 다는 뜻
+sheetCount = workbook.nsheets 
 
-personNum = 0
+personNum = 0 #얘네가 어떻게 작동을 하는지 잘 모르겟음 ;
 personIdx = 2
 rowCount = 0
 wsheetList = workbook.sheets()
@@ -12,5 +12,5 @@ for worksheet in wsheetList :
     for row in range(1, worksheet.nrows) :
         personNum += int(worksheet.cell_value(row, personIdx))
 
-print("전체 가수그룹 인원 합계 : ", personNum)
+print("전체 가수그룹 인원 합계 : ", personNum) 
 print("가수그룹 인원 평균 : ", personNum/rowCount)
