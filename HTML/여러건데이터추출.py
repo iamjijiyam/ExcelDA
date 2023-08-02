eba8bae98691477c9489287750a9fd40eca485b0
jijiyam <malody17120@naver.com>
2023-08-02 13:44:31 +0900

    파일 경로 수정을 통해 오류 수정 완료

--- HTML/여러건데이터추출.py
+++ HTML/여러건데이터추출.py
@@ -1,13 +1,13 @@
 import bs4
 
-webPage = open('Sample03.html', 'rt', encoding='utf-8').read()
+webPage = open('Sample02.html', 'rt', encoding='utf-8').read()
 bsObject = bs4.BeautifulSoup(webPage, 'html.parser')
 
-tag_ul= bsObject.find('ul')
-print(tag_ul)
+tag_ul= bsObject.find('ul') #<ul>태그의 모든 내용을 추출함. 
+print(tag_ul)               
 print()
 
-tag_ul= bsObject.find('li')
+tag_ul= bsObject.find('li')  #<ul>태그의 하위 항목인 li,들을 찾음 
 print(tag_li)
 print()
 
